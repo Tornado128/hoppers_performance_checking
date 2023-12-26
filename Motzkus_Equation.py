4adfadc8b62f971d4f0795bd491d811dc768d3c0
Yaser <yaserbozorgi@gmail.com>
2023-12-25 22:04:39 -0500

    Fixed some bugs

--- Motzkus_Equation.py
+++ Motzkus_Equation.py
@@ -5,91 +5,89 @@
 ## The reason for picking the numerical approach is that the parameters like wall friction angle, bulk density, ... are changing in the
 #vertical direction because of the increasing load
 
-#def Motzkus_Equation(X1,X2,Y1,Y2,N,sigmav_init):
-import numpy as np
-from curve_fitting import curve_fitting
-import math
-import matplotlib.pyplot as plt
+def Motzkus_Equation(X1,X2,Y1,Y2,N,sigmav_init):
+    import numpy as np
+    from curve_fitting import curve_fitting
+    import math
+    import matplotlib.pyplot as plt
 
-#Y = [0.325, 0]
-#X = [0.2845, 0.2]
-Y1 = 0.325
-Y2 = 0
-X1 = 0.28845
-X2 = 0.2
+    #Y = [0.325, 0]
+    #X = [0.2845, 0.2]
+    Y1 = 0.325
+    Y2 = 0
+    X1 = 0.28845
+    X2 = 0.2
 
-Y_apex = ((Y2-Y1)/(X2-X1))*(0-X1)+Y1                                                                                    # imaginary position of the apex of the cone (see Figure 4 of the reference)
+    Y_apex = ((Y2-Y1)/(X2-X1))*(0-X1)+Y1                                                                                    # imaginary position of the apex of the cone (see Figure 4 of the reference)
 
-N = 1000
+    g = 9.8                                                                                                             # gravity (m/s2)
+    #This function fits bulk density, effective angle of internal
+    # friction, FC and FFC vs sigma1.It also does a power fit for wall friction angle vs normal stress
+    # a, b and c are the coefficients for power and linear curve fittings.
+    [a, b, c] = curve_fitting()
 
-g = 9.8                                                                                                             # gravity (m/s2)
-#This function fits bulk density, effective angle of internal
-# friction, FC and FFC vs sigma1.It also does a power fit for wall friction angle vs normal stress
-# a, b and c are the coefficients for power and linear curve fittings.
-[a, b, c] = curve_fitting()
+    theta = (np.pi/2 - np.arctan((Y2-Y1)/(X2-X1)))*180/np.pi                                                            # hopper vertical angle (degree)
+    delY = (Y1 - Y2) / N                                                                                            # increment size in z direction (m)
+    sigmav = 0.1*np.ones(N + 1)                                                                                         # powder load in the vertical section of the hopper (pa) (it varies with z)
+    sigmav[0] = 50000
+    z_loc = np.zeros(N + 1)                                                                                             # increments in the vertical direction (m)
+    rhob = np.zeros(N)                                                                                                  # bulk density (kg/m3)
+    PHIE = np.zeros(N)                                                                                                  # effective angle of internal friction (degree)
+    UYS = np.zeros(N)                                                                                                   # unconfined yield strength or "FC" (pa)
+    PHILIN = np.zeros(N)                                                                                                # linearized angle of internal friction
+    WFA = np.zeros(N)                                                                                                   # wall friction angle
 
-theta = (np.pi/2 - np.arctan((Y2-Y1)/(X2-X1)))*180/np.pi                                                            # hopper vertical angle (degree)
-delY = (Y1 - Y2) / N                                                                                            # increment size in z direction (m)
-sigmav = 0.1*np.ones(N + 1)                                                                                         # powder load in the vertical section of the hopper (pa) (it varies with z)
-sigmav[0] = 50000
-z_loc = np.zeros(N + 1)                                                                                             # increments in the vertical direction (m)
-rhob = np.zeros(N)                                                                                                  # bulk density (kg/m3)
-PHIE = np.zeros(N)                                                                                                  # effective angle of internal friction (degree)
-UYS = np.zeros(N)                                                                                                   # unconfined yield strength or "FC" (pa)
-PHILIN = np.zeros(N)                                                                                                # linearized angle of internal friction
-WFA = np.zeros(N)                                                                                                   # wall friction angle
+    for i in range(N):
+        ## parameters for the Janssen equation: parameters needed to estimate an initial guess for sigmav for implementation of implicit Euler method
+        rhob[i] = a[0]*sigmav[i]**b[0]+c[0]
+        PHIE[i] = a[1] * sigmav[i] ** b[1] + c[1]
+        UYS[i] = a[2]*sigmav[i] + b[2]
+        PHILIN[i] = a[3]+sigmav[i] + b[3]
+        WFA[i] = a[4]*sigmav[i] ** b[4] + c[4]
+        if (WFA[i] > 85):
+            WFA[i] = 85                                 # Wall friction angle can not be above 85 degrees; otherwise it is a big number
 
-for i in range(N):
-    ## parameters for the Janssen equation: parameters needed to estimate an initial guess for sigmav for implementation of implicit Euler method
-    rhob[i] = a[0]*sigmav[i]**b[0]+c[0]
-    PHIE[i] = a[1] * sigmav[i] ** b[1] + c[1]
-    UYS[i] = a[2]*sigmav[i] + b[2]
-    PHILIN[i] = a[3]+sigmav[i] + b[3]
-    WFA[i] = a[4]*sigmav[i] ** b[4] + c[4]
-    if (WFA[i] > 85):
-        WFA[i] = 85                                 # Wall friction angle can not be above 85 degrees; otherwise it is a big number
+        ## Evaluation of K
+        # Eq. (12) of the reference
 
-    ## Evaluation of K
-    # Eq. (12) of the reference
+        if (WFA[i]>PHIE[i]):
+            WFA[i] = PHIE[i]                            # Wall friction angle may be unphysically larger than effective angle of internal friction at very low stresses (close to the top of the powder)
 
-    if (WFA[i]>PHIE[i]):
-        WFA[i] = PHIE[i]                            # Wall friction angle may be unphysically larger than effective angle of internal friction at very low stresses (close to the top of the powder)
+        lambda_if = (1 - np.sin(math.radians(WFA[i])) ** 2.0 - np.sqrt(
+            (1 - np.sin(math.radians(WFA[i])) ** 2) * (np.sin(math.radians(PHIE[i])) ** 2 - np.sin(math.radians(WFA[i])) ** 2))) / (
+                            1 + np.sin(math.radians(WFA[i])) ** 2.0 + np.sqrt((1 - np.sin(math.radians(WFA[i])) ** 2) * (
+                            np.sin(math.radians(PHIE[i])) ** 2 - np.sin(math.radians(WFA[i])) ** 2)))
 
-    lambda_if = (1 - np.sin(math.radians(WFA[i])) ** 2.0 - np.sqrt(
-        (1 - np.sin(math.radians(WFA[i])) ** 2) * (np.sin(math.radians(PHIE[i])) ** 2 - np.sin(math.radians(WFA[i])) ** 2))) / (
-                        1 + np.sin(math.radians(WFA[i])) ** 2.0 + np.sqrt((1 - np.sin(math.radians(WFA[i])) ** 2) * (
-                        np.sin(math.radians(PHIE[i])) ** 2 - np.sin(math.radians(WFA[i])) ** 2)))
+        # Eq. (15) of the reference
+        lambda_f = (np.tan(math.radians(90 - theta)) - np.tan(math.radians(WFA[i])) - lambda_if * np.tan(math.radians(WFA[i])) * (1 + np.tan(math.radians(WFA[i])) ** 2.0 -(np.tan(math.radians(90 - theta)) - np.tan(math.radians(WFA[i]))) ** 2)) / (np.tan(math.radians(90 - theta)) * (1 + np.tan(math.radians(WFA[i])) * np.tan(math.radians(90 - theta))))
+        mu_f = (lambda_if / lambda_f) * np.tan(math.radians(WFA[i]))
 
-    # Eq. (15) of the reference
-    lambda_f = (np.tan(math.radians(90 - theta)) - np.tan(math.radians(WFA[i])) - lambda_if * np.tan(math.radians(WFA[i])) * (1 + np.tan(math.radians(WFA[i])) ** 2.0 -(np.tan(math.radians(90 - theta)) - np.tan(math.radians(WFA[i]))) ** 2)) / (np.tan(math.radians(90 - theta)) * (1 + np.tan(math.radians(WFA[i])) * np.tan(math.radians(90 - theta))))
-    mu_f = (lambda_if / lambda_f) * np.tan(math.radians(WFA[i]))
+        if (theta > 90 - math.degrees(np.arcsin(np.sin(math.radians(WFA[i])) / np.sin(math.radians(PHIE[i]))))):
+            # Eq. (11) of the reference
+            K = 0.5 * (1 + lambda_if) - 0.5 * (1 - lambda_if) * np.cos(2 * math.radians(theta)) + lambda_if * np.tan(math.radians(WFA[i])) * np.sin(2 * math.radians(theta))
+        else:
+            # Eq. (14) of the reference
+            K = 0.5 * (1 + lambda_f) - 0.5 * (1 - lambda_f) * np.cos(2 * math.radians(theta)) + mu_f * lambda_f * np.sin(2 * math.radians(theta))
 
-    if (theta > 90 - math.degrees(np.arcsin(np.sin(math.radians(WFA[i])) / np.sin(math.radians(PHIE[i]))))):
-        # Eq. (11) of the reference
-        K = 0.5 * (1 + lambda_if) - 0.5 * (1 - lambda_if) * np.cos(2 * math.radians(theta)) + lambda_if * np.tan(math.radians(WFA[i])) * np.sin(2 * math.radians(theta))
-    else:
-        # Eq. (14) of the reference
-        K = 0.5 * (1 + lambda_f) - 0.5 * (1 - lambda_f) * np.cos(2 * math.radians(theta)) + mu_f * lambda_f * np.sin(2 * math.radians(theta))
+        # Eq. (8) of the reference
+        n = 2 * (K * (1 + np.tan(math.radians(WFA[i])) / np.tan(math.radians(theta))) - 1)
 
-    # Eq. (8) of the reference
-    n = 2 * (K * (1 + np.tan(math.radians(WFA[i])) / np.tan(math.radians(theta))) - 1)
+        sigmav_guess = (g * rhob[i] - n*sigmav[i]/((Y1-Y_apex-z_loc[i])))*delY + sigmav[i]                             # We use explicit euler method to estimate sigma0 provide an initial guess for the implicit euler method!
+        j = 0                                                                                                           # numerator for the while loop
+        error_percent = 100                                                                                             # the goal is to bring the error percent to below 0.1 percent for the convergence
+        # while loop checks that error percent (between our initial guess and the estimation) is below 0.1%
+        while (error_percent>0.1 and j<100):
+            rhob[i] = a[0] * sigmav_guess ** b[0] + c[0]
+            PHIE[i] = a[1] * sigmav_guess ** b[1] + c[1]
+            UYS[i] = a[2] * sigmav_guess + b[2]
+            PHILIN[i] = a[3] + sigmav_guess + b[3]
+            WFA[i] = a[4] * sigmav_guess ** b[4] + c[4]
+            sigmav[i+1] = (g * rhob[i] - n*sigmav_guess/((Y1-Y_apex-z_loc[i])))*delY + sigmav[i]
+            error_percent = 100*abs( (sigmav_guess - sigmav[i + 1]) / sigmav_guess)
+            sigmav_guess = sigmav[i+1]
 
-    sigmav_guess = (g * rhob[i] - n*sigmav[i]/((Y1-Y_apex-z_loc[i])))*delY + sigmav[i]                             # We use explicit euler method to estimate sigma0 provide an initial guess for the implicit euler method!
-    j = 0                                                                                                           # numerator for the while loop
-    error_percent = 100                                                                                             # the goal is to bring the error percent to below 0.1 percent for the convergence
-    # while loop checks that error percent (between our initial guess and the estimation) is below 0.1%
-    while (error_percent>0.1 and j<100):
-        rhob[i] = a[0] * sigmav_guess ** b[0] + c[0]
-        PHIE[i] = a[1] * sigmav_guess ** b[1] + c[1]
-        UYS[i] = a[2] * sigmav_guess + b[2]
-        PHILIN[i] = a[3] + sigmav_guess + b[3]
-        WFA[i] = a[4] * sigmav_guess ** b[4] + c[4]
-        sigmav[i+1] = (g * rhob[i] - n*sigmav_guess/((Y1-Y_apex-z_loc[i])))*delY + sigmav[i]
-        error_percent = 100*abs( (sigmav_guess - sigmav[i + 1]) / sigmav_guess)
-        sigmav_guess = sigmav[i+1]
-
-        j = j + 1
-    z_loc[i+1] = (i+1) * delY
+            j = j + 1
+        z_loc[i+1] = (i+1) * delY
     return sigmav
 #print(sigmav[N])
 ##plt.plot(sigmav, z_loc, 'b-')
