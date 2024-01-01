f2b5a15a65e909ea505a61227a40e10a7d919a50
Tornado128 <yaserbozorgi@gmail.com>
2023-12-31 23:13:35 -0500

    modified the code to avoid numerical glitch

--- MassFlow_or_FunnelFlow.py
+++ MassFlow_or_FunnelFlow.py
@@ -1,5 +1,5 @@
 
-def MassFlow_or_FunnelFlow(X1, X2, Y1, Y2, sigmav, sigma1, PHIE, rhob, WFA, UYS, sigmaf, N, number,RADIUS):
+def MassFlow_or_FunnelFlow(X1, X2, Z1, Z2, sigmav, sigma1, PHIE, rhob, WFA, UYS, sigmaf, N, number,RADIUS):
 
     import numpy as np
     from curve_fitting import curve_fitting
@@ -7,15 +7,16 @@ def MassFlow_or_FunnelFlow(X1, X2, Y1, Y2, sigmav, sigma1, PHIE, rhob, WFA, UYS,
 
     # Eq. (5) and Eq. (6) in Leung et al, J. pharmaceutical sciences, 108 (2019) 464-475
     if (WFA[-1]>PHIE[-1]):
-        WFA[-1] = PHIE[-1]          #To avoid nan to estimate beta in the next line
+        WFA[-1] = PHIE[-1]          #To avoid nan in the estimation of beta in the next line
     beta = (WFA[-1] + (180 / np.pi) * np.arcsin(np.sin(WFA[-1] * np.pi / 180) / np.sin(PHIE[-1] * np.pi / 180))) / 2
     theta_critical = 90 - 0.5 * (180 / np.pi) * np.arccos((1 - np.sin(PHIE[-1] * np.pi / 180)) / (2 * np.sin(PHIE[-1] * np.pi / 180))) - beta
+    print(PHIE[-1])
+    ## angle of the outlet of the hopper from a vertical line
+    # (I added 0.000000000000001 to the denominator to avoid having the denominator equal to zero)
+    theta = 90 - abs(np.arctan((Z2 - Z1) / (X2 - X1 + 0.000000000000001))) * 180 / np.pi
 
-    # angle of the outlet of the hopper from a vertical line (I added 0.000000000000001 to the denominator to avoid having the denominator equal to zero)
-    theta = 90 - np.arctan((Y2 - Y1) / (X2 - X1 + 0.000000000000001)) * 180 / np.pi
-
-    F = -1                                          # F=1 is funnel flow and F=0 is mass flow in the passive state
-    P = -1                                          # If F=0, P=0 is no-arch and P=1 is equivalent to arch formation
+    F = -1                                          # Initialization of F: Later, F=1 is funnel flow and F=0 is mass flow in the passive state
+    P = -1                                          # Initialization of P: Later, If F=0, P=0 means no arch forms while P=1 is equivalent to arch formation
     if (theta>theta_critical):
         F = 1                                       # funnel flow
     else:
@@ -24,20 +25,21 @@ def MassFlow_or_FunnelFlow(X1, X2, Y1, Y2, sigmav, sigma1, PHIE, rhob, WFA, UYS,
     if (F == 0):                                    # if there is a mass flow, determine if there is a risk of arch formation
 
         # B is the outlet diameter of the hopper (m)
-        B = 2*RADIUS
+        B = 2 * X2
+        #B = 2*RADIUS
 
         # Eq. 20 and 21 in Leung et al, J. pharmaceutical sciences, 108 (2019) 464-475
-        # X and Y are just two parameters needed to estimate the major principal stress at the outlet of the hopper
+        # X and Z are just two parameters needed to estimate the major principal stress at the outlet of the hopper
         # in the passive state ("sigma1_out")
         X = (2 * np.sin(math.radians(PHIE[-1])) / (1 - np.sin(math.radians(PHIE[-1])))) * (
                     np.sin(math.radians(2 * beta + theta)) / (np.sin(math.radians(theta)) + 1))
-        Y = ((2 * (1 - np.cos(math.radians(beta + theta)))) * np.sin(math.radians(theta)) + np.sin(
+        Z = ((2 * (1 - np.cos(math.radians(beta + theta)))) * np.sin(math.radians(theta)) + np.sin(
             math.radians(beta)) * np.sin(math.radians(beta + theta)) ** 2) / (
                         1 - np.sin(math.radians(PHIE[-1])) * np.sin(math.radians(beta + theta)) ** 3)
 
         #Eq. (19) in Leung et al, J. pharmaceutical sciences, 108 (2019) 464-475.
         #sigma1_out is the major principal stress at the outlet of the hopper in the passive state
-        sigma1_out = (1 + np.sin(math.radians(PHIE[-1]))) * Y * (rhob[-1]) * 9.8 * B / (
+        sigma1_out = (1 + np.sin(math.radians(PHIE[-1]))) * Z * (rhob[-1]) * 9.8 * B / (
                     2 * (X - 1) * np.sin(math.radians(theta)))
 
         # UYS_out is the unconfined yield strength (or cohesive strength) of the powder at the outlet of the hopper
